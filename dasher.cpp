44cf071eb7421b5dc46706856e2b15d70314d1dd
Eduardo Zaniboni <eduardowz.prog@gmail.com>
2024-02-19 21:51:13 -0300

    Test commit

--- dasher.cpp
+++ dasher.cpp
@@ -8,16 +8,61 @@ int main()
   // initialize window
   InitWindow(windowWidth, windowHeight, "Dapper Dasher");
 
-  SetTargetFPS(60);
+  // acceleration the to gravity (pixels/frame)/frame
+  const int gravity{1};
 
+  Texture2D scarfy = LoadTexture("textures/scarfy.png");
+  Rectangle scarfyRec;
+  scarfyRec.width = scarfy.width / 6;
+  scarfyRec.height = scarfy.height;
+  scarfyRec.x = 0;
+  scarfyRec.y = 0;
+  Vector2 scarfyPos;
+  scarfyPos.x = windowWidth / 2 - scarfy.width / 2;
+  scarfyPos.y = windowHeight / 2 - scarfyRec.height;
+
+  // is the rectangle in the air?
+  bool isInAir{};
+  // jump velocity
+  const int jumpVelocity{-15};
+
+  int velocity{0};
+
+  SetTargetFPS(60);
   while (!WindowShouldClose())
   {
     // start drawing
     BeginDrawing();
     ClearBackground(WHITE);
 
+    // perform ground check
+    if (scarfyPos.y >= windowHeight - scarfyRec.height)
+    {
+      // rectangle is on the ground
+      velocity = 0;
+      isInAir = false;
+    }
+    else
+    {
+      // rectangle is in the air
+      velocity += gravity;
+      isInAir = true;
+    }
+
+    // jump check
+    if (IsKeyPressed(KEY_SPACE) && !isInAir)
+    {
+      velocity += jumpVelocity;
+    }
+
+    // update the position
+    scarfyPos.y += velocity;
+
+    DrawTextureRec(scarfy, scarfyRec, scarfyPos, WHITE);
+
     // stop drawing
     EndDrawing();
   }
+  UnloadTexture(scarfy);
   CloseWindow();
 }
